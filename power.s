f09295a8987c95f01faa4fd2cf10d6de5bf21e55
Konstantin <turbo.foxy01@gmail.com>
2026-09-08 00:06:24 +0200

    add more comments, improve calling conventions, fix 0 exponent edge-case

--- power.s
+++ power.s
@@ -1,3 +1,11 @@
+# ***************************************************************************
+#
+# * Program: Power calculator
+# * Description: This program takes two inputs from the user 
+#   (exponent and base) and outputs the result.
+# 
+#***************************************************************************
+
 .data
 
     result: .asciz "\nResult: %u\n"
@@ -7,6 +15,15 @@
 
 .text
 
+# ***************************************************************************
+#
+# * Subroutine: exp_base_input 
+# * Description: This coroutine asks the user for an exponent and a base 
+#   and stores them in registers:
+#   base (as a qword) -> %rdx 
+#   expontent (as a qword) -> %rax 
+# 
+#***************************************************************************
 exp_base_input:
     #Prologue
     pushq %rbp
@@ -22,7 +39,7 @@ exp_base_input:
     movq $input, %rdi
     leaq -8(%rbp), %rsi
     call scanf
-    movq -8(%rbp), %rbx
+    //movq -8(%rbp), %rbx
 
     #Prompt user for base
     movq $base_prompt, %rdi
@@ -32,28 +49,50 @@ exp_base_input:
     movq $input, %rdi
     leaq -16(%rbp), %rsi
     call scanf
-    movq -16(%rbp), %rax
+    //movq -16(%rbp), %rax
+
+    popq %rax 
+    popq %rdx
 
     #Epilogue
     movq %rbp, %rsp
     popq %rbp
     ret
 
+# ***************************************************************************
+#
+# * Coroutine: pow 
+# * Description: This coroutine takes two arguments and computes an exponent (base^exp)
+#   
+# * Arguments:
+#   base -> %rbp (as qword)
+#   exp -> %rsi (as qword)
+#   
+# * Output:  
+#   %rax
+#
+#***************************************************************************
 pow:
     #Prologue
     pushq %rbp
     movq %rsp, %rbp
-    movq %rdi, %rax
-    movq %rsi, %rcx
+    movq %rdi, %rax # Moving the base parameter into %rax
+    movq %rsi, %rcx # Moving the exp parameter into %rcx (counter)
 
+    #Edge case where exponent is 0
+    cmp $0, %rcx
+    je clean
 #Multiply base by itself exp times
 iter:
-    cmp $1, %rcx
+    cmp $1, %rcx  # Check whether the count is 1
     je done
-    mulq %rdi
-    dec %rcx
-    jmp iter
+    mulq %rdi   # If count is not yet 1: %rax * %rdi -> %rax
+    dec %rcx    # Decrement counter
+    jmp iter    # Loop
 
+#Clean Up
+clean:
+    movq $1, %rax #Only reachable if the exponent is 0
 done:
     #Epilogue
     movq %rbp, %rsp
@@ -67,16 +106,19 @@ main:
     push %rbp
     mov %rsp, %rbp
     
-    call exp_base_input
+    call exp_base_input 
 
+
+    #Supply parameter to pow
     movq %rax, %rdi   #base -> rdi
-    movq %rbx, %rsi   #exponent -> rsi
+    movq %rdx, %rsi   #exponent -> rsi
 
     call pow
 
     #Print result
-    mov %rax, %rsi
-    mov $result, %rdi
+    mov %rax, %rsi      # Copy the result of pow to %rsi (1st parameter for printf)
+    mov $result, %rdi   # Copy result text to %rdi (2nd parameter to printf)
+    mov $0, %rax        # No args for printf
     call printf
 
     #Epilogue
