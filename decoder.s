6b22e1ed2e972780bc733bf72f201cdab06c0569
Konstantin <turbo.foxy01@gmail.com>
2026-09-15 01:05:27 +0200

    implementation with an external buffer

--- decoder.s
+++ decoder.s
@@ -1,6 +1,11 @@
+.bss
+    BUFFER: .skip 1024	#reserve 1024 bytes for printf
+	
 .text
 
-.include "dcd_msg/helloWorld.s"
+.include "final.s"
+
+
 
 .global main
 
@@ -19,28 +24,78 @@ decode:
 	# prologue
 	pushq	%rbp 			# push the base pointer (and align the stack)
 	movq	%rsp, %rbp		# copy stack pointer value to base pointer
+	movq $0, %r8 			# set index = 0 for the first iteration
+
+	movq $8, %r11			#size of 1 chunk
+
+	#movq (%rdi), %rsi	# load the first quadword of the message into rsi
+
+	movq $BUFFER, %rsi	#store the pointer to the last character in the buffer
 	
-	movq +1(%rdi), %rsi
-
-	movq %rsi, %rcx
-
-	shl $48, %rcx			# -> rcx ammount of characters
-	shr $56, %rcx			
-
 	
 
+	section_loop:
+	
+	movq %rdi, %r10 	# cur_pointer = data[0]   (data pointer for block with index 0)
+
+	movq %r8, %rax		
+	mulq %r11			#calculate index * 8 (move the pointer 8 bytes based on the index)
+
+
+	addq %rax, %r10	# data_ptr + index * 8	
+						# cur_pointer = data[index] (adress)
+
+	#movq (%r10), %rsi	# tmp = data[index] (value)
+
+	movq (%r10), %r8
+	shl $16, %r8
+	shr $32, %r8	# get index and store in r8
+		
+
+	movq (%r10), %rcx
+	shl $48, %rcx
+	shr $56, %rcx	# get amount and store in rcx
+
+			
+	movq (%r10), %r9
+	shl $56, %r9
+	shr $56, %r9	# get char and store in r9
+			
+		
+		
+	#rcx -> amount
+	#r8	-> index
+	#r9 ->char
 
 	decode_iter:
 		cmp $0, %rcx
 		je	exit_loop
 		
-
+		#push character to the buffer
+		movb %r9b, (%rsi) #move the character to the buffer
+		addq $1, %rsi	 #increment the pointer to the next free space
+		
 		loop decode_iter
+		
 
 
 	exit_loop:
+	
+	cmp $0, %r8
+	je end_decode	#if next index is 0 finish decoding
 
+	jmp section_loop
 
+	end_decode:
+	#push terminator character to the buffer
+	movb $0, (%rsi) 
+	addq $1, %rsi
+
+	#print buffer characters
+	movq $0, %rax
+	movq $BUFFER, %rdi		#start to print from the bottom of the stack
+	call printf
+	
 
 
 	# epilogue
